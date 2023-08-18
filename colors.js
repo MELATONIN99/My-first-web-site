ac2f17ba0c24c839036a21967f15fd3491732e1b
MELATONIN99 <140742210+MELATONIN99@users.noreply.github.com>
2023-08-18 16:22:56 +0900

    My-first-web-site 개인피드백2차( LocalStorage를 이용해 밤낮버튼 유지,시계 기능 추가)

--- colors.js
+++ colors.js
@@ -1,11 +1,5 @@
 var Links = {
     setColor:function(color){
-    // var alist = document.querySelectorAll('a');
-    // var i = 0;
-    // while(i < alist.length){
-    // alist[i].style.color = color;
-    // i = i + 1;
-    //   } 
       $('a').css('color', color);
      }
     }
@@ -21,38 +15,43 @@ var Links = {
             function nightDayHandler(event) {
                 const bodyElement = document.querySelector('body');
                 bodyElement.classList.toggle('night');
-
                 const button = event.target;
-              //         if(button.value ==='밤낮'){
-              //           value = "day"
-                      
-              //         } 
-              //         else {
-              //         const bodyElement = document.querySelector('body');
-              //         bodyElement.classList('night')  
-              //          value = "night"
-                
-                
-              //   }
-              // }
-              if(button.value !=='밤낮'){
-              const bodyElement = document.querySelector('body');
-              bodyElement.classList('night')  
-               value = "night"
-        
-        
-        }
-      }
+                if (bodyElement.classList.contains('night')) {
+                  localStorage.setItem('밤낮저장', 'night');
+              } else {
+                  localStorage.setItem('밤낮저장', 'day');
+              }
+          }
+          document.addEventListener('DOMContentLoaded', function() {
+            const savedValue = localStorage.getItem('밤낮저장');
+            const bodyElement = document.querySelector('body');
+            
+            if (savedValue === 'night') {
+                bodyElement.classList.add('night');
+            }
+        });
+            
+        //       // if(button.value !=='밤낮'){
+        //       bodyElement.classList('night')  
+        //        value = "night"
+        //         localStorage.removeItem('밤낮저장','밤낮저어장');
+        // }
+      // }
+
+// CLOCK SCRIPT
+const clock = document.querySelector("#clock")
+
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+// getClock 을 바로 불러줘야 1초뒤에 시계가 작동하는게아닌
+// 불러와서 바로작동가능
+getClock();
+setInterval(getClock, 1000);
+
 
     
-    // function nightDayHandler(){
-    //     var target = document.querySelector('body');
-    //       if(self.value ==='night'){
-    //       self.value ='day';
-          
-    
-
-        
-    // } else {
-    // self.value ='night';
-    // }}
